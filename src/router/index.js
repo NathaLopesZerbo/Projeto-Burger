73c3e1cfd013912ce7b25026c467562566f78461
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-12 14:12:21 -0300

    Entendendo sobre o vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import About from '../views/Pedidos.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/about', name: 'About', component: About }
+  { path: '/pedidos', name: 'Pedidos', component: About }
 ]
 
 const router = createRouter({
