b6065eec260eeb6ea14e2f4a56a65498ad702bab
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-12 15:32:26 -0300

    Etilizando a aplicação

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
     <Banner />
     <div class="main-container">
         <h1>Monte o seu Burger:</h1>
-        <p>Formulário</p>
+        <BurgerForm />
     </div>
 </template>
 
 <script>
+    import BurgerForm from '../components/BurgerForm.vue';
     import Banner from '../components/Banner.vue';
 
     export default{
         name: "Home",
         components:{
-            Banner
+            Banner,
+            BurgerForm
         }
     }
 </script>
