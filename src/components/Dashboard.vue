871142530f96295a5bfb6123f1088779effc24bf
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-13 11:42:23 -0300

    Inserindo algumas funcionalidades diretamente para o sistema

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -24,10 +24,10 @@
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updateBurger($event, burger.id)">
                         <option value="">Selecione</option>
                         <option v-for="s in status" :key="s.id" 
-                        value="s.tipo" :selected="burger.status == s.tipo">
+                        :value="s.tipo" :selected="burger.status == s.tipo">
                         {{ s.tipo }}
                         </option>
                     </select>
@@ -72,7 +72,22 @@
                 const res = await req.json();
 
                 this.getPedidos();
-            }
+            },
+            async updateBurger(event, id){
+                const option = event.target.value;
+
+                const dataJson = JSON.stringify({status:option});
+
+                const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                    method: 'PATCH',
+                    headers: {"Content-Type": "application/json"},
+                    body: dataJson
+                });
+
+                const res = await req.json();
+
+                console.log(res)
+            }   
         },
         mounted(){
             this.getPedidos();
