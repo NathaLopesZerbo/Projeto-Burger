652d96e551510c86a9698808d9ef441f3a523361
Nathã Lopes Zerbo <nathalopeszerbo7@gmail.com>
2025-06-13 11:27:33 -0300

    Inserindo algumas funções CRUD na aplicação

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,22 +11,27 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>Nome do Cliente</div>
-                <div>Tipo de Pão</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{burger.nome }}</div>
+                <div> {{ burger.pao }}</div>
+                <div> {{ burger.carne }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Tomate</li>
+                        <li v-for="(opcional, index) in burger.opcionais" :key="index">
+                            {{ opcional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option v-for="s in status" :key="s.id" 
+                        value="s.tipo" :selected="burger.status == s.tipo">
+                        {{ s.tipo }}
+                        </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
         </div>
@@ -34,10 +39,49 @@
 </template>
 
 <script>
+
     export default{
-        name: 'Dashboard'
+        name: 'Dashboard',
+        data() {
+            return {
+                burgers: null,
+                burger_id: null,
+                status: [],
+            }
+        },
+        methods:{
+            async getPedidos(){
+                const req = await fetch('http://localhost:3000/burgers');
+
+                const data = await req.json();
+
+                this.burgers = data;
+
+                this.getStatus();
+            },
+            async getStatus(){
+                const req = await fetch('http://localhost:3000/status');
+                const data = await req.json();
+                this.status = data;
+            },
+            async deleteBurger(id){
+                const req  = await fetch(`http://localhost:3000/burgers/${id}`,{
+                    method: 'DELETE'
+                });
+
+                const res = await req.json();
+
+                this.getPedidos();
+            }
+        },
+        mounted(){
+            this.getPedidos();
+        }
     }
 </script>
+
+
+
 
 <style scoped>
     #burger-table {
